5b0773c51646ca3cbb78655c4c7ace240b426c94
berlianristy <berlianristypratiwii@gmail.com>
2021-10-01 02:12:40 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/clothic/myfavorite.java
@@ -0,0 +1,14 @@
+package com.example.clothic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myfavorite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myfavorite);
+    }
+}
